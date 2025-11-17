043c5a4d397ab56bd12e7a72b53b8c9b28a9b6ef
dnlbui <dnlbui@proton.me>
2025-11-17 07:12:57 -0600

    Add expo-image-picker and expo-image-loader dependencies; implement image selection functionality in the app. Update Button and ImageViewer components to support image picking.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,13 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 type Props = {
   label: string;
   theme?: 'primary';
+  onPress?: () => void;
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === 'primary') {
     return (
       <View
@@ -17,7 +18,7 @@ export default function Button({ label, theme }: Props) {
       >
         <Pressable
           style={[ styles.button, { backgroundColor: '#fff' }]}
-          onPress={() => alert('You pressed a button.')}
+          onPress={onPress || (() => alert('You pressed a button.'))}
         >
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
@@ -27,7 +28,7 @@ export default function Button({ label, theme }: Props) {
   }
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={onPress || (() => alert('You pressed a button.'))}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
