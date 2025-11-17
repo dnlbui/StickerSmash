999bd6b73fd450027671d8322a9baae7102223cf
dnlbui <dnlbui@proton.me>
2025-11-17 07:57:02 -0600

    Add emoji selection functionality with new components for emoji picker, list, and sticker. Enhance image editing options with reset and save features in the main interface.

--- components/Button.tsx
+++ components/Button.tsx
@@ -18,7 +18,7 @@ export default function Button({ label, theme, onPress }: Props) {
       >
         <Pressable
           style={[ styles.button, { backgroundColor: '#fff' }]}
-          onPress={onPress || (() => alert('You pressed a button.'))}
+          onPress={onPress}
         >
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
@@ -28,7 +28,7 @@ export default function Button({ label, theme, onPress }: Props) {
   }
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={onPress || (() => alert('You pressed a button.'))}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
